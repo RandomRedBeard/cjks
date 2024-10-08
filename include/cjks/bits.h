48a735788cb858d9400c94af09c162a310936928
Thomas <thomas@Thomass-MacBook-Pro.local>
2024-10-08 13:10:06 -0700

    Entering the hellscape of endianness (for the last time?)

--- include/cjks/bits.h
+++ include/cjks/bits.h
@@ -1,16 +1,21 @@
 #ifndef BITS_CJKS_H
 #define BITS_CJKS_H
 
-#ifdef _WIN32
-#include <WinSock2.h>
-#define cjks_ntohs_f ntohs
-#define cjks_ntohi_f ntohl
-#define cjks_ntohll_f ntohll
-#else
+#ifdef HAS_ENDIAN_H
 #include <endian.h>
 #define cjks_ntohs_f be16toh
 #define cjks_ntohi_f be32toh
 #define cjks_ntohll_f be64toh
+#elif defined _WIN32
+#include <WinSock2.h>
+#define cjks_ntohs_f ntohs
+#define cjks_ntohi_f ntohl
+#define cjks_ntohll_f ntohll
+#elif defined __APPLE__ // No endian.h and not win32
+#include <libkern/OSByteOrder.h>
+#define cjks_ntohs_f OSSwapBigToHostInt16
+#define cjks_ntohi_f OSSwapBigToHostInt32
+#define cjks_ntohll_f OSSwapBigToHostInt64
 #endif
 
 #include <cjks/lib.h>
