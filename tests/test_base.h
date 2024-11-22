d75ca2b2f5deee25f3a95ce8805c34740565635d
Thomas <tnj@17cosmicln.net>
2024-11-22 08:06:05 -0800

    CRT memcheck added

--- tests/test_base.h
+++ tests/test_base.h
@@ -9,6 +9,20 @@
 #define CJKS_RES_DIR "resources"
 #endif
 
+#ifdef _WIN32
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+
+#define memcheckinit \
+_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE); \
+_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
+
+#define memcheckfin     _CrtDumpMemoryLeaks()? assert(0):assert(1);
+#else
+#define memcheckinit
+#define memcheckfin
+#endif
+
 #include <assert.h>
 #include <stdio.h>
 
@@ -19,9 +33,11 @@ typedef struct {
 } test_st;
 
 static void cjks_run_tests(test_st* tests) {
+    memcheckinit
     while (tests->name) {
         printf("Running test %s\n", tests->name);
         tests->test();
+        memcheckfin
         printf("Test completed %s\n", tests->name);
         tests++;
     }
