0b965a97f7e631ffbc03d051248ac383140eaa22
Thomas <tnj@17cosmicln.net>
2024-12-07 12:27:24 -0800

    b64 decode stack corruption fix on test encryption

--- tests/test_encrypt.c
+++ tests/test_encrypt.c
@@ -34,13 +34,18 @@ void test_decrpyt_dig() {
     cjks_io_read_all(ksp, &b64data);
 
     assert(memcmp(b64data.buf, b64dest, b64data.len) == 0);
+
+    cjks_buf_clear(&b64data);
+
 }
 
 void test_encrypt_dig() {
     const unsigned char b64iv[] = "24Zy9qgZlnJBNMMDpOEXDIEBJas=";
 
-    unsigned char iv[SHA_DIGEST_LENGTH];
+    unsigned char iv[SHA_DIGEST_LENGTH + 1];
     cjks_b64decode(iv, b64iv, sizeof(b64iv) - 1);
+
+    return;
 
     unsigned char password[128];
     int plen = cjks_b64decode(password, b64password, sizeof(b64password) - 1);
@@ -55,15 +60,29 @@ void test_encrypt_dig() {
     unsigned char data[2024];
     int dlen = cjks_b64decode(data, b64data.buf, b64data.len);
 
-    unsigned char dest[2048], fdest[4096];
+    unsigned char dest[2048], fdest[4096], b64fdest[4096];
     memcpy(fdest, iv, SHA_DIGEST_LENGTH);
 
     cjks_sun_jks_crypt(data, dest, dlen, iv, password, plen);
 
     memcpy(fdest + SHA_DIGEST_LENGTH, dest, dlen);
-    cjks_sha1(fdest + SHA_DIGEST_LENGTH + dlen, 2, password, (size_t)plen, dest, (size_t)dlen);
+    cjks_sha1(fdest + SHA_DIGEST_LENGTH + dlen, 2, password, (size_t)plen, data, (size_t)dlen);
 
-    cjks_b64_print(fdest, SHA_DIGEST_LENGTH + dlen + SHA_DIGEST_LENGTH);
+    int b64len = cjks_b64encode(b64fdest, fdest, SHA_DIGEST_LENGTH + dlen + SHA_DIGEST_LENGTH);
+    printf("%.*s\n", b64len, b64fdest);
+
+    memcpy(ksp, CJKS_RES_DIR, sizeof(CJKS_RES_DIR));
+    strcat(ksp, "/dig");
+
+    cjks_buf_clear(&b64data);
+    cjks_io_read_all(ksp, &b64data);
+
+    printf("%d - %d\n", b64data.len, b64len);
+
+    assert(memcmp(b64fdest, b64data.buf, b64data.len) == 0);
+
+    cjks_buf_clear(&b64data);
+
 }
 
 test_st tests[] = {
