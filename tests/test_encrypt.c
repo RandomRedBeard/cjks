6379e6024dfd54d9d34b5fea9e3587f86a28776f
Thomas <thomas@Thomass-MacBook-Pro.local>
2024-12-07 11:50:48 -0800

    More tests

--- tests/test_encrypt.c
+++ tests/test_encrypt.c
@@ -4,9 +4,9 @@
 
 #include "test_base.h"
 
-unsigned char b64password[] = "AGMAaABhAG4AZwBlAGkAdA==";
+void test_decrpyt_dig() {
+    const unsigned char b64password[] = "AGMAaABhAG4AZwBlAGkAdA==";
 
-int main() {
     char ksp[128];
     memcpy(ksp, CJKS_RES_DIR, sizeof(CJKS_RES_DIR));
     strcat(ksp, "/dig");
@@ -20,12 +20,26 @@ int main() {
     unsigned char password[128];
     int plen = cjks_b64decode(password, b64password, sizeof(b64password) - 1);
 
-    char dest[2048], *pkey_ptr = dest;
+    char dest[2048], b64dest[4096];
 
     int r = cjks_sun_jks_decrypt(data, dest, dlen, password, plen);
-    printf("%d\n", r);
+    cjks_b64encode(b64dest, dest, dlen - 40);
 
-    cjks_b64_print(dest, dlen - 40);
+    memcpy(ksp, CJKS_RES_DIR, sizeof(CJKS_RES_DIR));
+    strcat(ksp, "/d.key");
 
+    cjks_buf_clear(&b64data);
+    cjks_io_read_all(ksp, &b64data);
+
+    assert(memcmp(b64data.buf, b64dest, b64data.len) == 0);
+}
+
+test_st tests[] = {
+    {"decrypt_dig", test_decrpyt_dig},
+    {NULL, NULL}
+};
+
+int main() {
+    cjks_run_tests(tests);
     return 0;
 }
