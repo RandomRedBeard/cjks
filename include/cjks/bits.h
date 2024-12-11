32d18c5654b4eefc19bc8643e01ffa1c1f8ef112
Thomas <tnj@17cosmicln.net>
2024-12-11 08:41:36 -0800

    Starting to write pk

--- include/cjks/bits.h
+++ include/cjks/bits.h
@@ -7,21 +7,27 @@
     #define cjks_ntohi be32toh
     #define cjks_ntohll be64toh
 
+    #define cjks_htons htobe16
     #define cjks_htoni htobe32
+    #define cjks_htonll htobe64
 #elif defined _WIN32 // Windows
     #include <WinSock2.h>
     #define cjks_ntohs ntohs
     #define cjks_ntohi ntohl
     #define cjks_ntohll ntohll
 
+    #define cjks_htons htons
     #define cjks_htoni htonl
+    #define cjks_htonll htonll
 #elif defined __APPLE__ // No endian.h and not win32
     #include <libkern/OSByteOrder.h>
     #define cjks_ntohs OSSwapBigToHostInt16
     #define cjks_ntohi OSSwapBigToHostInt32
     #define cjks_ntohll OSSwapBigToHostInt64
 
+    #define cjks_htons OSSwapHostToBigInt16
     #define cjks_htoni OSSwapHostToBigInt32
+    #define cjks_htonll OSSwapHostToBigInt64
 #endif
 
 #endif
---
+++ tests/test_write_pk.c
@@ -0,0 +1,9 @@
+#include "test_base.h"
+
+void test_pk_write() {
+    // <uint32 eber len><eber><uint32 chain len><uint16 cert type len><cert type><uint32 cert len><cert>...
+}
+
+CJKS_TESTS_ST
+CJKS_TEST(test_pk_write)
+CJKS_TESTS_END
