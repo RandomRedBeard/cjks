c9e19c6047b227b36fae10be90dd103a9e1cb308
Thomas <thomas@Thomass-MacBook-Pro.local>
2024-12-15 08:18:42 -0500

    Slowly moving towards sha io impl

--- tests/test_sha1.c
+++ tests/test_sha1.c
@@ -1,17 +1,15 @@
 #include "test_base.h"
 #include <cjks/utl.h>
+#include <cjks/io.h>
 #include "private/debug.h"
 
 #define cir_ls(i, n) ((i << n) | (i >> (32 - n)))
 
-typedef struct block_st {
-    uint32 words[16];
-    uint32 i;
-} block;
-
 typedef struct hash_st {
     uint32 h[5];
     uint64 len;
+    uint32 words[16];
+    uint32 i;
 } hash;
 
 void init_hash(hash* hs) {
@@ -22,11 +20,12 @@ void init_hash(hash* hs) {
     hs->h[4] = 0xC3D2E1F0;
 
     hs->len = 0;
+    hs->i = 0;
 }
 
-void hash_block(hash* hs, block* blk) {
+void hash_block(hash* hs) {
     for (int i = 0; i < 16; i++) {
-        blk->words[i] = cjks_htoni(blk->words[i]);
+        hs->words[i] = cjks_htoni(hs->words[i]);
     }
 
     uint32 a, b, c, d, e, f, k, tmp;
@@ -40,8 +39,8 @@ void hash_block(hash* hs, block* blk) {
     for (uchar i = 0; i < 80; i++) {
         if (i > 15) {
             // W(t-3) XOR W(t-8) XOR W(t-14) XOR W(t-16)
-            blk->words[i & 0x0F] = blk->words[i - 3 & 0x0F] ^ blk->words[i - 8 & 0x0F] ^ blk->words[i - 14 & 0x0F] ^ blk->words[i - 16 & 0x0F];
-            blk->words[i & 0x0F] = cir_ls(blk->words[i & 0x0F], 1);
+            hs->words[i & 0x0F] = hs->words[i - 3 & 0x0F] ^ hs->words[i - 8 & 0x0F] ^ hs->words[i - 14 & 0x0F] ^ hs->words[i - 16 & 0x0F];
+            hs->words[i & 0x0F] = cir_ls(hs->words[i & 0x0F], 1);
         }
 
         if (i < 20) {
@@ -62,7 +61,7 @@ void hash_block(hash* hs, block* blk) {
         }
 
         // TEMP = S^5(A) + f(t;B,C,D) + E + W(t) + K(t);
-        tmp = cir_ls(a, 5) + f + e + blk->words[i & 0x0F] + k;
+        tmp = cir_ls(a, 5) + f + e + hs->words[i & 0x0F] + k;
 
         // E = D;  D = C;  C = S^30(B);  B = A; A = TEMP;
         e = d;
@@ -80,53 +79,56 @@ void hash_block(hash* hs, block* blk) {
     hs->h[4] += e;
 }
 
-void process_message(hash* hs, block* blk, uchar* msg, size_t len) {
+void process_message(hash* hs, uchar* msg, size_t len) {
     hs->len += len;
     for (int i = 0; i < len; i++) {
-        ((uchar*)blk)[blk->i++] = msg[i];
-        if (blk->i == 64) {
-            hash_block(hs, blk);
-            blk->i = 0;
+        ((uchar*)&hs->words)[hs->i++] = msg[i];
+        if (hs->i == 64) {
+            hash_block(hs);
+            hs->i = 0;
         }
     }
 }
 
-void finalize_hash(hash* hs, block* blk) {
-    uchar* bptr = (uchar*)&blk->words + blk->i;
-    *bptr = 0x80;
-    memset(bptr + 1, 0, sizeof(blk->words) - blk->i - 1);
+void finalize_hash(hash* hs) {
+    uchar* bptr = (uchar*)&hs->words + hs->i;
+    *bptr++ = 0x80;
+    hs->i++;
 
-    if (blk->i > 55) {
-        hash_block(hs, blk);
-        memset(&blk->words, 0, sizeof(blk->words));
+    if (hs->i > 56) {
+        memset(bptr, 0, 64 - hs->i);
+        hash_block(hs);
+        memset(&hs->words, 0, sizeof(hs->words));
+    }
+    else {
+        memset(bptr, 0, 58 - hs->i);
     }
 
     uint64 len = cjks_htonll(hs->len * 8);
-    memcpy(&blk->words[14], &len, sizeof(len));
+    memcpy(&hs->words[14], &len, sizeof(len));
 
-    hash_block(hs, blk);
+    hash_block(hs);
+
+    for (int i = 0; i < 5; i++) {
+        hs->h[i] = cjks_ntohi(hs->h[i]);
+    }
 }
 
 int main() {
     hash hs;
     init_hash(&hs);
 
-    block blk;
-    blk.i = 0;
-
     FILE* fp = fopen("README.md", "rb");
-    uchar buf[128];
+    uchar buf[90];
     size_t rlen = 0;
-    while ((rlen = fread(buf, 1, sizeof(buf), fp)) > 0) {
-        process_message(&hs, &blk, buf, rlen);
-    }
+    // while ((rlen = fread(buf, 1, sizeof(buf), fp)) > 0) {
+    //     process_message(&hs, buf, rlen);
+    // }
+
+    process_message(&hs, (uchar*)"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", 57);
 
     // Padding
-    finalize_hash(&hs, &blk);
-
-    for (int i = 0; i < 5; i++) {
-        hs.h[i] = cjks_ntohi(hs.h[i]);
-    }
+    finalize_hash(&hs);
 
     hexprint((uchar*)&hs, 20);
     printf("Done\n");
