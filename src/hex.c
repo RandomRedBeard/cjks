946c663424485d2275176bb69bead6704b54a441
Thomas <tnj@17cosmicln.net>
2025-01-19 09:53:16 -0800

    hex touch

--- src/hex.c
+++ src/hex.c
@@ -6,7 +6,7 @@ char cjks_v2a(int c) {
 
 int cjks_hex(char *dest, const uchar *src, size_t len) {
     for (size_t i = 0; i < len; i++) {
-        *dest++ = cjks_v2a((src[i] >> 4) & 0x0F);
+        *dest++ = cjks_v2a(src[i] >> 4);
         *dest++ = cjks_v2a((src[i]) & 0x0F);
     }
     return (int)(len * 2);
